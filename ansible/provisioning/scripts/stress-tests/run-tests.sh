5e1ade982d924759797f8d1e45309624abacab66
TomeMD <tome.maseda@udc.es>
2024-05-06 14:55:47 +0200

    Added slurm job sbatch script

--- ansible/provisioning/scripts/stress-tests/run-tests.sh
+++ ansible/provisioning/scripts/stress-tests/run-tests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 OUTPUT_FILE="${SLURM_JOB_PARTITION}.log"
+SBATCH_PID_FILE=/tmp/${SLURM_JOB_ID}_pid
+if [ -f "${SBATCH_PID_FILE}" ]; then
+    SBATCH_PID=$(cat ${SBATCH_PID_FILE})
+fi
 
 sleep 10
 
@@ -34,3 +38,10 @@ mkdir -p $OUTPUT_DIR
 ./run.sh -b ${SLURM_JOB_PARTITION} -o $OUTPUT_DIR -v apptainer -w stress-system --stressors iomix --other-options temp-path=/scratch2
 cp $OUTPUT_FILE $OUTPUT_DIR
 cat /dev/null > $OUTPUT_FILE
+
+sleep 60
+
+if [ -z "${SBATCH_PID}" ]; then
+    rm "${SBATCH_PID_FILE}"
+    kill -SIGTERM ${SBATCH_PID}
+fi
